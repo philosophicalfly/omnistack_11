6c7c68082d24f4bf227d4d0600fc53c34c0e2847
Augusto Bardini <azbardini@gmail.com>
2020-03-25 08:44:26 -0300

    :sparkles: Add Index and Routes

---
+++ backend/src/routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const NgoController = require('./controllers/NgoController');
+const routes = express.Router();
+
+const connection = require('./database/connection');
+
+routes.post('/index', (req, res) => {
+  // const params = req.query;
+  // const params = req.params;
+  // const body = req.body;
+  return res.json({
+    event: 'Omnistack',
+    aluno: 'Bardini'
+  });
+});
+
+routes.post('/ngo', NgoController.create);
+routes.get('/ngo', NgoController.list)
+routes.get('/delNgo', NgoController.remove)
+
+module.exports = routes;
+
+/*
+Methods:
+GET: Find/Search info
+POST: Create info
+PUT: Update info
+DELETE: Delete info
+*/
+
+/*
+Parameter types:
+QUERY params: Parameters send in route (filters, pagination) after ?  | '/test/'
+route PARAMS: Parameters used to identify resources                   | '/test/:id'
+request body: Body of requesr, create or update resources
+*/
