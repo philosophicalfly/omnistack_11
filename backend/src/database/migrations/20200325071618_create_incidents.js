b130162333a26e54f66af72d6818c8bfa446354b
Augusto Bardini <azbardini@gmail.com>
2020-03-25 08:44:44 -0300

    :sparkles: Add database relates stuff

---
+++ backend/src/database/migrations/20200325071618_create_incidents.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('incident', table => {
+    table.increments;
+
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
+
+    table.string('ngo_id').notNullable();
+
+    table.foreign('ngo_id').references('id').inTable('ngo');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('incident');
+};
