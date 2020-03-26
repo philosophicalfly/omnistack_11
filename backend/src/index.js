9a7c8447a233cbe1a95e84dc00544e3475b30398
Augusto Bardini <azbardini@gmail.com>
2020-03-26 02:43:42 -0300

    :sparkles: Add Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json())
 app.use(routes);
+app.use(cors());
 
 app.listen(3030)
